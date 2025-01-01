96099a65c92ab4ed87d77a71f72a91e9c0b4dbcb
Alexandre GARCIN <perso@alex-home.fr>
2025-01-01 20:51:58 +0100

    Fix hover issue when edding nav label and hovering #36

--- src/Utils/Input.tsx
+++ src/Utils/Input.tsx
@@ -1,10 +1,9 @@
 import { HTMLInputTypeAttribute, useRef, useState, useEffect } from 'react';
 
-export const Input = ({ className, active, _default, placeholder, pattern, type, inputMode, validate, reset, onChange }: {
+export const Input = ({ className, active, _default, pattern, type, inputMode, validate, reset, onChange }: {
    active: boolean,
    className?: string,
    _default?: string,
-   placeholder?: string,
    pattern?: string,
    inputMode?: "email" | "search" | "tel" | "text" | "url" | "none" | "numeric" | "decimal",
    validate?: (_value: string) => boolean,
@@ -19,6 +18,9 @@ export const Input = ({ className, active, _default, placeholder, pattern, type,
    useEffect(() => {
       if (reset) {
          setValue(_default ?? "");
+         if (ref.current) {
+            ref.current.value = "";
+         }
       }
    }, [reset, setValue, _default]);
 
@@ -29,13 +31,14 @@ export const Input = ({ className, active, _default, placeholder, pattern, type,
    return <div className={"group overflow-hidden px-4 grow bg-gray-700 p-1 shadow-lg flex text-left text-white rounded-sm border-2 border-gray-900 " + (className ?? "")
       + (active ? ' hocus:bg-gray-800 hocus:drop-shadow-xl hocus:border-msfs has-[:focus]:border-msfs has-[:hover]:border-msfs' : ' opacity-30')
       + (valid ? '' : ' invalid')}>
-      <input ref={ref} type={type} className={'grow flex overflow-hidden bg-transparent ' + (valid ? '' : ' invalid')} disabled={!active} value={value} inputMode={inputMode} placeholder={placeholder} pattern={pattern}
+      <input ref={ref} type={type} className={'grow flex overflow-hidden bg-transparent ' + (valid ? '' : ' invalid')} disabled={!active} placeholder={value} inputMode={inputMode} pattern={pattern}
          onChange={e => {
             if (validate?.(e.target.value) ?? true) {
                setValue(e.target.value);
                setValid(true);
             } else {
                setValid(false);
+               ref.current!.value = value;
             }
          }} />
    </div>;
